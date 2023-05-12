b295c01eb732f486f5747623adaea9da90eab23a
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-05-12 18:30:38 +0300

    add react navigation libraries

---
+++ screens/MapScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+function MapScreen(props) {
+  return (
+    <View style={styles.container}>
+      <Text>Holla, I am the map screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default MapScreen;
