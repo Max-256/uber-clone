9810955ad960d5354088ff99b1b4a0e1e0cfab64
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-05-13 15:38:15 +0300

    add gogle places auto complete to nav card

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,16 +1,74 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, SafeAreaView } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
 
 function NavigateCard(props) {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
   return (
-    <View style={styles.container}>
-      <Text>Nav card</Text>
-    </View>
+    <SafeAreaView style={styles.fullContainer}>
+      <Text style={styles.text}>Good day, Maxwell</Text>
+      <View>
+        <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              backgroundColor: "white",
+              paddingTop: 20,
+              flex: 0,
+            },
+            textInput: {
+              backgroundColor: "#DDDDDF",
+              borderRadius: 5,
+              fontSize: 18,
+            },
+            textInputContainer: {
+              paddingHorizontal: 15,
+              paddingBottom: 0,
+            },
+          }}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setDestination({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+
+            navigation.navigate("RideOptions");
+          }}
+          minLength={2}
+          placeholder="Where to?"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          enablePoweredByContainer={false}
+          fetchDetails={true}
+          returnKeyType={"search"}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  fullContainer: {
+    backgroundColor: "#fff",
+    flex: 1,
+  },
+  text: {
+    textAlign: "center",
+    paddingVertical: 10,
+    fontSize: 20,
+    fontWeight: "500",
+  },
 });
 
 export default NavigateCard;
