f652f46924ef6d17d6cb4f3248eeecf09c65bd4f
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-05-13 21:54:55 +0300

    fit map to supplied markers

--- components/Map.js
+++ components/Map.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { useSelector } from "react-redux";
@@ -10,9 +10,28 @@ import { GOOGLE_MAPS_APIKEY } from "@env";
 function Map(props) {
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
+  const mapRef = useRef();
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    var i = setInterval(() => {
+      mapRef?.current?.fitToSuppliedMarkers(["origin", "destination"], {
+        edgePadding: {
+          top: 100,
+          right: 100,
+          left: 100,
+          bottom: 100,
+          aimated: true,
+        },
+      });
+      clearInterval(i);
+    }, 50);
+  }, [origin, destination]);
 
   return (
     <MapView
+      ref={mapRef}
       style={styles.map}
       mapType="standard"
       initialRegion={{
@@ -40,6 +59,17 @@ function Map(props) {
           title="Origin"
           description={origin.description}
           identifier="origin"
+        />
+      )}
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title="Destination"
+          description={destination.description}
+          identifier="destination"
         />
       )}
     </MapView>
