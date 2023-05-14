240d3b229e00a7e55a4ae25f3897a090a76490c6
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-05-14 20:52:46 +0300

    add back navigation to ride options

--- components/RideOptions.js
+++ components/RideOptions.js
@@ -1,16 +1,50 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 function RideOptions(props) {
+  const navigation = useNavigation();
+
   return (
-    <View style={styles.container}>
-      <Text>Pick a ride</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Ionicons
+          onPress={() => navigation.goBack()}
+          style={styles.back}
+          name="chevron-back-circle-outline"
+          size={30}
+        />
+
+        <Text style={styles.selectRide}>Select a ride</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  back: {
+    position: "absolute",
+    top: 10,
+    left: 15,
+    zIndex: 1,
+  },
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+  },
+  selectRide: {
+    textAlign: "center",
+    paddingVertical: 13,
+    fontSize: 20,
+    verticalAlign: "middle",
+  },
 });
 
 export default RideOptions;
